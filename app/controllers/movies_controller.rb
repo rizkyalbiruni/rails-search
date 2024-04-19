53d513ee48793f830ffe07034213925327172a7c
Muhammad Rizky <rizky.wazza@gmail.com>
2024-04-19 23:14:35 +0200

    instead of db movies come directly from api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,16 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = []
     if params[:query].present?
-      @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%")
+      uri = URI("https://www.omdbapi.com/?s=#{params[:query]}&apikey=adf1f2d7") # replace with your API endpoint
+      response = Net::HTTP.get(uri)
+      @data = JSON.parse(response)
+      @movies = @data['Search'] || []
+      #  raise
     end
   end
 end
