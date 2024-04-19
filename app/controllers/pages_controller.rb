e631b6d9cc958baad76175cd4317534a5c4b5494
Muhammad Rizky <rizky.wazza@gmail.com>
2024-04-19 15:59:29 +0200

    homepage has movies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+    @movies = Movie.all
+    if params[:query].present?
+      @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%")
+    end
   end
 end
