84190aba4167ae5026f8c0c2ea3d77d4d8f3e5ef
Muhammad Rizky <rizky.wazza@gmail.com>
2024-04-21 12:35:36 +0200

    pages feature added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,16 @@
-require 'net/http'
-require 'uri'
-require 'json'
+# require 'net/http'
+# require 'uri'
+# require 'json'
 
-class MoviesController < ApplicationController
-  def index
-    @movies = []
-    if params[:query].present?
-      uri = URI("https://www.omdbapi.com/?s=#{params[:query]}&apikey=adf1f2d7") # replace with your API endpoint
-      response = Net::HTTP.get(uri)
-      @data = JSON.parse(response)
-      @movies = @data['Search'] || []
-      #  raise
-    end
-  end
-end
+# class MoviesController < ApplicationController
+#   def index
+#     @movies = []
+#     if params[:query].present?
+#       uri = URI("https://www.omdbapi.com/?s=#{params[:query]}&apikey=adf1f2d7") # replace with your API endpoint
+#       response = Net::HTTP.get(uri)
+#       @data = JSON.parse(response)
+#       @movies = @data['Search'] || []
+#       #  raise
+#     end
+#   end
+# end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,12 @@ require 'json'
 class PagesController < ApplicationController
   def home
     @movies = []
+    @page = params[:page] || 1
     if params[:query].present?
-      uri = URI("https://www.omdbapi.com/?s=#{params[:query]}&apikey=adf1f2d7") # replace with your API endpoint
+      uri = URI("https://www.omdbapi.com/?s=#{params[:query]}&page=#{@page}&apikey=adf1f2d7") # replace with your API endpoint
       response = Net::HTTP.get(uri)
       @data = JSON.parse(response)
-      @movies = @data['movies'] || []
+      @movies = @data['Search'] || []
       # raise
     end
   end
