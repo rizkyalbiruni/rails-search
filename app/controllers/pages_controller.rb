88d819107fd7224731233414d5a48875dd0aa564
Muhammad Rizky <rizky.wazza@gmail.com>
2024-04-21 13:14:07 +0200

    pages added results added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ require 'json'
 class PagesController < ApplicationController
   def home
     @movies = []
+    @data = {}
     @page = params[:page] || 1
     if params[:query].present?
       uri = URI("https://www.omdbapi.com/?s=#{params[:query]}&page=#{@page}&apikey=adf1f2d7") # replace with your API endpoint
